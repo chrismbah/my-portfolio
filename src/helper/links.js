7994b420949af06ebab48bc3fefe7ae302fb3039
chrismbah <mbahchris46@gmail.com>
2025-03-15 11:33:16 +0100

    change resume link

--- src/helper/links.js
+++ src/helper/links.js
@@ -1,5 +1,5 @@
 export const myLinks = {
-    resume: "https://docs.google.com/document/d/1lUcUAYqEyum1JkEaHaXwDv0hZcpxN-5ORN2rWwvhcPc/edit?usp=sharing",
+    resume: "https://docs.google.com/document/d/1Bva0QVLEu0UBMizIVBJ-e-zCasg5wGZzmtdmmF5GKB4/edit?usp=sharing",
     gmail: "mailto:mbahchris46@gmail.com",
     github: "https://github.com/chrismbah",
     linkedin: "https://linkedin.com/in/mbah-chris",
